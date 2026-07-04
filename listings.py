d177469ec64c1fcd721d8cab47f093fecc60b178
iradeokule-art <ira.deokule@sjsu.edu>
2026-07-04 16:35:00 -0700

    Update listings.py

--- listings.py
+++ listings.py
@@ -78,6 +78,11 @@ for col in cols:
     print(f" Mode:   {data.mode()[0]:,.2f}")
     print(f" 1st Quartile: {data.quantile(0.25):,.2f}")
     print(f" 3rd Quartile: {data.quantile(0.75):,.2f}")
+    outliers = data[(data > high) | (data < low)]
+    print(f" Outlier count: {len(outliers)}")
+    print(f" Outlier values: {outliers.values}")
+    print(f" Outlier minimum: {outliers.min()}")
+    print(f" Outlier maximum: {outliers.max()}")
 listings.to_csv("listings.csv", index=False)
 print("\n -- columns above 90% null---")
 totalCount = len(listings)
